ed8ce7c80903d28fa8f7616b595ad0a127bfa044
Javier Romero Rodriguez <javierybromero@gmail.com>
2022-09-03 19:12:07 +0200

    App completed

--- button-challenge/src/App.js
+++ button-challenge/src/App.js
@@ -16,10 +16,11 @@ function App() {
           <p className="navbar-text">Grid</p>
         </div>
       </div>
-      <div className="main-div">
+      <div>
+        <div className="main-container">
         <h3>Buttons</h3>
-        <div className="button-container">
-          <section className="main-buttons">
+    
+        <section className="button-layout">
             <div className="button-div">
               <p>{"<Button />"}</p>
               <Button >Default</Button>
@@ -45,11 +46,91 @@ function App() {
               <Button disabled>Disabled</Button>
             </div>
 
-          </section>
-        </div>
-        <footer>
+            <div className="button-div">
+              <p>{"<Button disabled variant=\"outline\"/>"}</p>
+              <Button disabled variant="outline">Disabled</Button>
+            </div>
 
+        </section>
+        <section className="button-layout">
+            <div className="button-div">
+              <p>{"<Button startIcon=\"settings\"/>"}</p>
+              <Button startIcon="settings"> &nbsp; Default </Button>
+            </div>
+            
+            <div className="button-div">
+              <p>{"<Button endIcon=\"settings\"/>"}</p>
+              <Button endIcon="settings"> Default &nbsp;</Button>
+            </div>
+
+            <div className="button-div">
+              <p>{"<Button startIcon=\"star\" variant=\"text\"/>"}</p>
+              <Button startIcon="star" variant="text"> &nbsp; Default </Button>
+            </div>
+            
+            <div className="button-div">
+              <p>{"<Button endIcon=\"star\" variant=\"outline\" disabled/>"}</p>
+              <Button endIcon="star" variant="outline" disabled> Disabled &nbsp;</Button>
+            </div>
+        </section>
+        <section className="button-layout">
+            <div className="button-div">
+              <p>{"<Button size=\"sm\"/>"}</p>
+              <Button size="sm"> Default </Button>
+            </div>
+            
+            <div className="button-div">
+              <p>{"<Button size=\"md\"/>"}</p>
+              <Button size="md"> Default </Button>
+            </div>
+
+            <div className="button-div">
+              <p>{"<Button size=\"lg\"/>"}</p>
+              <Button size="lg"> Default </Button>
+            </div>
+        </section>
+        <section className="button-layout">
+            <div className="button-div">
+              <p>{"<Button color=\"default\"/>"}</p>
+              <Button color="default"> Default </Button>
+            </div>
+            
+            <div className="button-div">
+              <p>{"<Button color=\"primary\"/>"}</p>
+              <Button color="primary"> Default </Button>
+            </div>
+
+            <div className="button-div">
+              <p>{"<Button color=\"secondary\"/>"}</p>
+              <Button color="secondary"> Default </Button>
+            </div>
+
+            <div className="button-div">
+              <p>{"<Button color=\"danger\"/>"}</p>
+              <Button color="danger"> Default </Button>
+            </div>
+        </section>
+        <section className="button-layout">
+            <div className="button-div">
+              <p>{"<Button color=\"danger\" variant=\"outline\" size=\"lg\"/>"}</p>
+              <Button color="danger" variant="outline" size="lg"> Default </Button>
+            </div>
+            
+            <div className="button-div">
+              <p>{"<Button color=\"secondary\" variant=\"text\" startIcon=\"close\"/>"}</p>
+              <Button color="secondary" variant="text" startIcon="close"> Default </Button>
+            </div>
+
+            <div className="button-div">
+              <p>{"<Button color=\"primary\" endIcon=\"autorenew\"/>"}</p>
+              <Button color="primary" endIcon="autorenew"> Default </Button>
+            </div>
+        </section>
+        <footer>
+          <p>Icons: https://material.io/resources/icons/?style=round</p>
+          <p id="creator-msg">created by Javier Romero - devChallenges.io</p>
         </footer>
+        </div>
       </div>
     </div>
   );
--- button-challenge/src/Button.jsx
+++ button-challenge/src/Button.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Button.css";
 
-function Button({ variant, disableShadow, disabled, children }) {
+function Button({ variant, disableShadow, disabled, children, startIcon, endIcon, size, color }) {
     return(
         <div className={`default ${variant ? `${variant}` : ""} 
         ${disableShadow ? "no-shadow" : ""}
-        ${disabled ? "disabled" : ""}`}>
+        ${disabled ? "disabled" : ""}
+        ${(startIcon || endIcon) ? "modified" : ""}
+        ${size==="sm" ? "small" : (size==="lg" ? "large" : "")}
+        ${color==="primary" ? "clr-primary" : (color==="secondary" ? "clr-secondary" : (color==="danger" ? "clr-danger" : ""))}`}>
+            <span class="material-symbols-outlined">{startIcon}</span>
             {children}
+            <span class="material-symbols-outlined">{endIcon}</span>
         </div>
     );
 }
